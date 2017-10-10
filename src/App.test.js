878b680f910868a132e6026b18a39905350d4e42
Walid Wahed <wwahed1@gmail.com>
2017-10-10 14:01:33 -0400

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,17 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Store from './App';
 import { shallow } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
 
 describe('Store', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<Store />));
+
   it('exists', () => {
     expect(Store).toBeDefined();
   });
 
   it('initializes with an empty cart', () => {
-    const wrapper = shallow(<Store />);
-    const actual = wrapper.instance().isProductInCart(1);
+    const actual = wrapper.state().productsInCart;
 
-    expect(actual).toBe(false);
+    expect(actual.length).toBe(0);
   });
+
+  it('finds a product by id', () => {
+    const actual = wrapper.instance().findProductById(1);
+    const firstProduct = {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 2};
+
+    expect(actual).toEqual(firstProduct);
+  });
+
+  describe('adding product to cart', () => {
+    beforeEach(() => wrapper.instance().handleAddToCart(1));
+
+    it('adds a product to the cart', () => {
+      const productInCart = {"id": 1, "title": "iPad 4 Mini", "price": 500.01, "inventory": 1};
+      expect(wrapper.state().productsInCart).toContainEqual(productInCart);      
+    });
+
+    it('decrements the inventory of product in product list', () => {
+      expect(wrapper.state().products[0].inventory).toBe(1);
+    });
+
+    it('keeps incrementing inventory of product in cart', () => {
+      wrapper.instance().handleAddToCart(1); // testing more than one
+      expect(wrapper.state().productsInCart[0].inventory).toBe(2);
+    });
+
+    describe('and then checks out', () => {
+      it('clears the cart when you check out', () => {
+        wrapper.instance().handleCheckout();
+
+        expect(wrapper.state().productsInCart.length).toBe(0);
+      });    
+    });
+  });
+
+  
 });
 
